c01fa67dae55da73a14fcb9789f2807248c4aaab
yzhu <zhuyiquan1998@gmail.com>
2016-05-23 12:49:56 -0400

    Minor Changes

--- ProgressBar/src/Tracker/ProgressTracker.java
+++ ProgressBar/src/Tracker/ProgressTracker.java
@@ -2,16 +2,17 @@ package Tracker;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.awt.Graphics;
 
 import com.IZ.core.AbstractGame;
 import com.IZ.core.GameContainer;
 import com.IZ.core.Renderer;
 
-public class ProgressTracker extends AbstractGame 
+public class ProgressTracker extends AbstractGame
 {
 	private static Timer timer;
 	private static TimerTask task;
-	
+
 	public static void main(String[] args)
 	{
 		GameContainer gc = new GameContainer(new ProgressTracker());
@@ -22,22 +23,21 @@ public class ProgressTracker extends AbstractGame
 		gc.setTitle("Timer & ScoreBoard Rendering Tester");
 		gc.setWidth(200);
 		gc.setHeight(200);
-		gc.setScale(4f);
+		gc.setScale(3f);
 		gc.setClearScreen(true);
-		gc.setDevMode(true);
 		gc.start();
 	}
 
 	@Override
-	public void update(GameContainer gameContainer, float dt) 
+	public void update(GameContainer gameContainer, float dt)
 	{
-		
+
 	}
 
 	@Override
-	public void render(GameContainer gameContainer, Renderer r) 
+	public void render(GameContainer gameContainer, Renderer r)
 	{
 		r.drawText("Time: " + TimeTracker.getTime(), 0xffff0000, 5, 5);
+		r.drawText("Bombs: ??", 0xffff0000, 155, 5);
 	}
-
 }
--- ProgressBar/src/Tracker/TimeTracker.java
+++ ProgressBar/src/Tracker/TimeTracker.java
@@ -5,19 +5,16 @@ import java.util.TimerTask;
 public class TimeTracker extends TimerTask
 {
 	private static int timeSecond = 0, timeMinute = 0;
-	private static String time = "0:0";
+	private static String time = "0:00";
 
 	@Override
 	public void run() 
 	{
 		timeSecond++;
 		
-		if(timeSecond == 60)
-		{
-			timeSecond = 0;
-			timeMinute ++;
-		}
-		time = timeMinute + ":" + timeSecond;
+		if(timeSecond == 60){timeSecond = 0;timeMinute ++;}
+		if(timeSecond < 10){time = timeMinute + ":0" + timeSecond;}
+		else{time = timeMinute + ":" + timeSecond;}
 	}
 
 	public static int getTimeSecond() 

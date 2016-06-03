d38ef292b06c6049fc2a3cca99bda1b99674b828
watersnake1 <mrmurray253@gmail.com>
2016-06-02 20:45:15 -0400

    updating master repo
    branches cannot automatically merge, so adding this manually

---
+++ src/Menu.java
@@ -0,0 +1,77 @@
+import sun.applet.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * Created by Christian on 6/1/16.
+ */
+public class Menu {
+   private JFrame frame;
+   private JPanel panel;
+   private ImageIcon titleIcon;
+   private JLabel titleLabel;
+   private MainFrame mainFrame;
+
+   public Menu() {
+      frame = new JFrame();
+      panel = new JPanel();
+      titleIcon = new ImageIcon("../images/MenuScreen.png");
+      titleLabel = new JLabel();
+      mainFrame = new MainFrame();
+   }
+
+   public void createAndShowGUI()
+   {
+      frame.setPreferredSize(new Dimension(640,360));
+      frame.add(panel);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      titleLabel.setIcon(titleIcon);
+      panel.add(titleLabel);
+      panel.updateUI();
+      frame.pack();
+      frame.setLocationRelativeTo(null);
+      frame.setVisible(true);
+      labelClickListener();
+   }
+
+   public void labelClickListener()
+   {
+      titleLabel.addMouseListener(new MouseListener() {
+         @Override
+         public void mouseClicked(MouseEvent e) {
+            MainFrame frame = new MainFrame();
+            frame.createAndShowGUI();
+
+         }
+
+         @Override
+         public void mousePressed(MouseEvent e) {
+
+         }
+
+         @Override
+         public void mouseReleased(MouseEvent e) {
+
+         }
+
+         @Override
+         public void mouseEntered(MouseEvent e) {
+
+         }
+
+         @Override
+         public void mouseExited(MouseEvent e) {
+
+         }
+      });
+   }
+
+   public static void main(String[] args)
+   {
+      Menu m = new Menu();
+      m.createAndShowGUI();
+   }
+}

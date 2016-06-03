2a1f638348171750a37b8e50eec530f550db666b
watersnake1 <mrmurray253@gmail.com>
2016-06-02 20:46:12 -0400

    Update MainFrame.java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -64,7 +64,7 @@ public class MainFrame
      **/
     public static void main(String[] args)
     {
-        MainFrame frame = new MainFrame();
-        frame.createAndShowGUI();
+        Menu m = new Menu();
+        m.createAndShowGUI();
     }
 }
